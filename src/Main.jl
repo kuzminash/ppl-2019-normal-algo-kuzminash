13fee7fe0fe0edc66f533085f3317fb31a3d512e
alexandrakuzmina <kuzminash@list.ru>
2019-09-15 15:35:50 +0300

    first

--- src/Main.jl
+++ src/Main.jl
@@ -1,1 +1,16 @@
-print("Hello world")
+function one_time(string, a1, a2)
+    string=replace(string, a1=>a2,count=1)
+    return string
+end
+function multiple_times(string, a1, a2)
+
+
+
+string=readline()
+algorithm1=[] #кладем элементы, которые будем заменять(переменная a1)
+algorithm2=[] #кладем элементы на которые будем заменять(переменная a2)
+algorithm3=[] #кладем 0 если подстановка однакратна и 1 если нет
+if occursin("aa", string)
+   string=one_time(string, "aa", "a")
+   println(string)
+end
