334138a18246c353c6bedacd4919ac36d53b98a6
alexandrakuzmina <kuzminash@list.ru>
2019-09-16 23:18:22 +0300

    first

--- src/Main.jl
+++ src/Main.jl
@@ -3,14 +3,20 @@ function one_time(string, a1, a2)
     return string
 end
 function multiple_times(string, a1, a2)
+    while occursin(a1, string)
+        string=replace(string, a1=>a2, count=1)
+    end
+    return string
+end
+#function multiple_times(string, a1, a2)
 
 
 
-string=readline()
+global string=readline()
 algorithm1=[] #кладем элементы, которые будем заменять(переменная a1)
 algorithm2=[] #кладем элементы на которые будем заменять(переменная a2)
 algorithm3=[] #кладем 0 если подстановка однакратна и 1 если нет
 if occursin("aa", string)
-   string=one_time(string, "aa", "a")
+    string=multiple_times(string, "aa", "a")
    println(string)
 end
