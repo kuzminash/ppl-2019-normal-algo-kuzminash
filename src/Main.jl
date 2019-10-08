d96cede4772bc3a584cfe79f76e060eee85ce5a2
alexandrakuzmina <kuzminash@list.ru>
2019-10-09 01:06:29 +0300

    first

--- src/Main.jl
+++ src/Main.jl
@@ -1,54 +1,86 @@
-function existance(string,exist,a1) ##функция возвращабщая переменную типа bool в зависимости от того нашлась ли подстрока
-  if occursin(a1, string)
-    exist=true
-  elseif a1=="•"
-    exist=true
-  else
-    exist=false
-  end
-  return exist
+function existance(string,first_substring)##функция возвращабщая переменную типа bool в зависимости от того нашлась ли подстрока
+      return occursin(first_substring, string)
 end
-function substitution(string, a1, a2) ##реализация замены подстроки
-    if a1!="•" && a2!="•"
-        string=replace(string, a1=>a2,count=1)
-    elseif  a1=="•"
-         string=a2*string
+
+function change_empty_sign(first_substring, second_substring)##заменяем в массиве знак пустой строки на пустую строку для дальнейшего упрощения
+    if first_substring==" "
+        first_substring=""
+    end
+    if second_substring==" "
+        second_substring=""
+    end
+    return first_substring, second_substring
+end
+
+function substitution(word, first_substring, second_substring) ##реализация замены подстроки
+    return replace(word, first_substring=>second_substring,count=1)
+end
+
+function if_found_substring(exist, word, first_substring, second_substring)
+    if exist
+        word=substitution(word,first_substring, second_substring)
+    end
+    return word
+end
+
+function read_from_the_file()
+    println("Напишите пиуть к файлу")
+    name_of_the_file=readline()
+    f = open(name_of_the_file, "r")
+    lines=readlines(f)
+    close(f)
+    return lines
+end
+
+function from_string_to_tuple(substitution)
+    left_part_of_substitution,righ_part_of_substitution=split(substitution,"=>")
+    if first(righ_part_of_substitution)=='.'
+        not_ultimate_substitution=false
+        righ_part_of_substitution=righ_part_of_substitution[2:end]
     else
-         string=replace(string, a1=>"",count=1)
+        not_ultimate_substitution=true
     end
-    return string
+    left_part_of_substitution,righ_part_of_substitution=change_empty_sign(left_part_of_substitution, righ_part_of_substitution)
+    println(left_part_of_substitution,righ_part_of_substitution,not_ultimate_substitution)
+    return left_part_of_substitution,righ_part_of_substitution,not_ultimate_substitution
 end
 
-
-
-println("Введите строку:")
-global string=readline()
-println("Введите количество подстановок:")
-substitutions=parse(UInt8, readline())
-println("Введите алгоритм следуя слудующим правилам:")
-println("Через пробел введите 3 значения. Первое-подстрока, которую хотите заменить, второе-подстрока на которую хотите заменить и третье-символ 0 если подстановка конечная и 1 если не конечная")
-println("Каждую последующую подстановку следует вводить с новой строки через enter")
-println("Пустую подстроку при вводе обозначать символом •  (вводится с клавиатуры как alt+8)")
-global algorithm=fill( "aaa" , (substitutions,3) ) ##в массиве будут хранится все подстановки (чтобы не оставлять массив пустым заполним его символами 'aaa')
-for i=1:substitutions ##
-    alg=readline()
-    algorithm[i,1], algorithm[i,2],algorithm[i,3]=split(alg," ")
+function split_substitutions(lines)
+ tuple_of_substitutions=Array{Tuple{String,String,Bool},1}()#заводим массив для подстановок
+ for number_of_the_substitution=1:length(lines)
+    println(from_string_to_tuple(lines[number_of_the_substitution]))
+    push!(tuple_of_substitutions,from_string_to_tuple(lines[number_of_the_substitution]))
+ end
+ return tuple_of_substitutions
 end
 
-exist=true
-
-while exist
-  for i=1:substitutions
-    global operation_number=i
-    global exist=existance(string,exist,algorithm[operation_number,1])
-    if exist==true
-        global string=substitution(string,algorithm[operation_number,1],algorithm[operation_number,2])
-        if algorithm[operation_number,3]=="0"
-            exist=false ##в случае конечной подстановки завершаем while
-        end
-        break
+function while_exist(word,lines,substitutions)
+    for i=1:length(lines)
+            operation_number=i
+            exist=existance(word,substitutions[operation_number][1])
+            word=if_found_substring(exist,word,substitutions[operation_number][1],substitutions[operation_number][2])
+            if !substitutions[operation_number][3]
+                return word, false
+             end
     end
-  end
+    return word, exist
 end
-println("Получившаяся строка:")
-println(string)
+
+function main()
+    lines=read_from_the_file()#считываем весь файл
+    word=lines[1]
+    lines=lines[2:end] #удаляем слово из нашего считанного файла, чтобы стались только подстановки
+    substitutions=split_substitutions(lines) #из файла делаем кортеж из подстановок
+    println(substitutions)
+    exist=true ##переменная отвечает за существование подстроки в строке
+
+    while exist  ##цикл будет существовать пока нашлась подстрока которую нужно заменить
+        word, exist=while_exist(word, lines,substitutions)
+    end
+    println("Получившееся слово:")
+    println(word)
+
+end
+
+main()
+
